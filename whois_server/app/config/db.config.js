09e0c2fad404d3ac3f0bfa27a92473638c724569
Alexander Ponomarev <dom2564@icloud.com>
2022-10-25 23:29:25 +0500

    add docker-compose

--- whois_server/app/config/db.config.js
+++ whois_server/app/config/db.config.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+console.log(process.env.DATABASE_HOST)
 module.exports = {
     HOST:  process.env.DATABASE_HOST,
     USER:  process.env.DATABASE_USERNAME,
     PASSWORD:  process.env.DATABASE_PASSWORD,
     DB:  process.env.DATABASE_NAME,
+    PORT: 5432,
     dialect: "postgres",
     pool: {
       max: 5,
@@ -12,3 +14,23 @@ module.exports = {
       idle: 10000
     }
   };
+
+//   const Sequelize = require('sequelize');
+
+//   //GET ENV VARIABLES FROM
+//   const sequelize = new Sequelize(
+//     process.env.DATABASE_NAME,
+//       process.env.DATABASE_USERNAME,
+//       process.env.DATABASE_PASSWORD,
+//       {
+//           host: process.env.DATABASE_HOST,
+//           dialect: 'postgres',
+//           pool: {
+//                   max: 5,
+//                   min: 0,
+//                   acquire: 30000,
+//                   idle: 10000
+//                 }
+//       });
+
+// module.exports = sequelize;
--- whois_server/app/models/index.js
+++ whois_server/app/models/index.js
@@ -1,4 +1,5 @@
 const dbConfig = require("../config/db.config.js");
+// const sequelize = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
