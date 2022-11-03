c5fbdfdd183be773c72dd60f674539c426c20e01
Alexander Ponomarev <dom2564@icloud.com>
2022-11-03 23:18:11 +0500

    bug fix

--- whois_client/src/http-common.js
+++ whois_client/src/http-common.js
@@ -1,6 +1,7 @@
 import axios from "axios"; 
 // const hostServe = "http://localhost:8080";
 // const hostServe = "http://5.53.124.242:8080"; IP_HOST
+console.log(process.env.IP_HOST + ":8080/api")
 export default axios.create({
   withCredentials: true,
   baseURL: process.env.IP_HOST + ":8080/api",
