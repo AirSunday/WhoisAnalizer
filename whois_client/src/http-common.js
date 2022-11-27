fccf94867404f557234073000ddbedfb546e7cc4
Alexander Ponomarev <dom2564@icloud.com>
2022-11-27 16:14:46 +0500

    create ssl

--- whois_client/src/http-common.js
+++ whois_client/src/http-common.js
@@ -1,10 +1,8 @@
 import axios from "axios";
-// const hostServe = "http://localhost:8080";
-// const hostServe = "http://5.53.124.242:8080"; IP_HOST
 export default axios.create({
   withCredentials: true,
   // baseURL: "http://localhost:8080/api",
-  baseURL: "http://188.68.222.76:8080/api",
+  baseURL: "https://188.68.222.76:8080/api",
   headers: {
     "Content-type": "application/json",
   },
