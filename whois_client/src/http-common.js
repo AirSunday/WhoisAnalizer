62082b96f0e96aa8b76bdef632992c4c00c5dc97
Alexander Ponomarev <dom2564@icloud.com>
2022-11-28 21:21:24 +0500

    create ssl

--- whois_client/src/http-common.js
+++ whois_client/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export default axios.create({
   withCredentials: true,
   // baseURL: "http://localhost:8080/api",
-  baseURL: "https://188.68.222.76:8080/api",
+  baseURL: "https://whoisa.ru/api",
   headers: {
     "Content-type": "application/json",
   },
