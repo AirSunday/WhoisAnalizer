df7c196367795602181bdab768f8b313876993a9
Alexander Ponomarev <dom2564@icloud.com>
2022-12-24 14:27:39 +0500

    restart server 3585000

--- whois_server/app/controllers/whois.controller.js
+++ whois_server/app/controllers/whois.controller.js
@@ -277,7 +277,7 @@ exports.GetStatistic = (req, res) => {
     })
       .then((data) => {
         const ageList = [
-          { value: "0 .. 1", count: 0 },
+          { value: "0 ... 1", count: 0 },
           { value: "2 .. 3", count: 0 },
           { value: "4 .. 6", count: 0 },
           { value: "7 .. 9", count: 0 },
